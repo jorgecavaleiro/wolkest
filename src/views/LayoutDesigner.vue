3e0e3ac96895443eba023f3e1ed9bc11be6e4441
Jorge Cavaleiro Dias <jorge.cavaleiro@gmail.com>
2022-06-05 10:57:20 +0100

    Bind all components palette

--- src/views/LayoutDesigner.vue
+++ src/views/LayoutDesigner.vue
@@ -1,6 +1,6 @@
 <template lang="en">
 <div class="container">
-   <div id="gjs" data-v-0ec79c91="" class="gjs-editor-cont" style="width: 100%; height:1280px;">
+   <div  data-v-0ec79c91="" class="gjs-editor-cont" style="width: 100%; height:1280px;">
       <div class="gjs-editor gjs-one-bg gjs-two-color">
          <div class="gjs-cv-canvas">
             <div class="gjs-cv-canvas__frames" data-frames="">
@@ -180,147 +180,19 @@
                <div v-if="selectedPanel==='Components'">
                   <div class="gjs-blocks-cs gjs-one-bg gjs-two-color">
                      <div class="gjs-block-categories">
-                        <div class="gjs-block-category" :class="{ 'gjs-open': componentsGroups[0].isOpen }" @click="componentsGroupToggle(0)">
+                        <div v-for="(group, index) in componentsGroups" :key="index" class="gjs-block-category" :class="{ 'gjs-open': componentsGroups[index].isOpen }" @click="componentsGroupToggle(index)">
                            <div class="gjs-title">
-                              <i class="gjs-caret-icon fa" :class="{ 'fa-caret-down': componentsGroups[0].isOpen, 'fa-caret-right': !componentsGroups[0].isOpen }" ></i>
-                              {{componentsGroups[0].title}}
+                              <i class="gjs-caret-icon fa" :class="{ 'fa-caret-down': componentsGroups[index].isOpen, 'fa-caret-right': !componentsGroups[index].isOpen }" ></i>
+                              {{componentsGroups[index].title}}
                            </div>
-                           <div v-if="componentsGroups[0].isOpen" class="gjs-blocks-c">
-                              <div class="gjs-fonts gjs-f-b1 gjs-block gjs-one-bg gjs-four-color-h" title="1 Column" draggable="true">
-                                 <div class="gjs-block-label">1 Column</div>
-                              </div>
-                              <div class="gjs-fonts gjs-f-b2 gjs-block gjs-one-bg gjs-four-color-h" title="2 Columns" draggable="true">
-                                 <div class="gjs-block-label">2 Columns</div>
-                              </div>
-                              <div class="gjs-fonts gjs-f-b3 gjs-block gjs-one-bg gjs-four-color-h" title="3 Columns" draggable="true">
-                                 <div class="gjs-block-label">3 Columns</div>
-                              </div>
-                              <div class="gjs-fonts gjs-f-b37 gjs-block gjs-one-bg gjs-four-color-h" title="2 Columns 3/7" draggable="true">
-                                 <div class="gjs-block-label">2 Columns 3/7</div>
-                              </div>
-                              <div class="gjs-fonts gjs-f-text gjs-block gjs-one-bg gjs-four-color-h" title="Text" draggable="true">
-                                 <div class="gjs-block-label">Text</div>
-                              </div>
-                              <div class="fa fa-link gjs-block gjs-one-bg gjs-four-color-h" title="Link" draggable="true">
-                                 <div class="gjs-block-label">Link</div>
-                              </div>
-                              <div class="gjs-fonts gjs-f-image gjs-block gjs-one-bg gjs-four-color-h" title="Image" draggable="true">
-                                 <div class="gjs-block-label">Image</div>
-                              </div>
-                              <div class="fa fa-youtube-play gjs-block gjs-one-bg gjs-four-color-h" title="Video" draggable="true">
-                                 <div class="gjs-block-label">Video</div>
-                              </div>
-                              <div class="fa fa-map-o gjs-block gjs-one-bg gjs-four-color-h" title="Map" draggable="true">
-                                 <div class="gjs-block-label">Map</div>
-                              </div>
-                              <div class="fa fa-link gjs-block gjs-one-bg gjs-four-color-h" title="Link Block" draggable="true">
-                                 <div class="gjs-block-label">Link Block</div>
-                              </div>
-                              <div class="fa fa-quote-right gjs-block gjs-one-bg gjs-four-color-h" title="Quote" draggable="true">
-                                 <div class="gjs-block-label">Quote</div>
-                              </div>
-                              <div class="gjs-fonts gjs-f-h1p gjs-block gjs-one-bg gjs-four-color-h" title="Text section" draggable="true">
-                                 <div class="gjs-block-label">Text section</div>
-                              </div>
+                           <div v-if="componentsGroups[index].isOpen" class="gjs-blocks-c">
+                              <div v-for="comp in componentsGroups[index].components" :key="comp.id" :class="comp.classes" class="gjs-block gjs-one-bg gjs-four-color-h" title="1 Column" draggable="true">
+                                 <span v-html="comp.innerHtml"></span>
+                                 <div class="gjs-block-label">{{comp.title}}</div>
+                              </div>                              
                            </div>
                         </div>
-                        <div class="gjs-block-category gjs-open">
-                           <div class="gjs-title">
-                              <i class="gjs-caret-icon fa fa-caret-down"></i>
-                              Extra
-                           </div>
-                           <div class="gjs-blocks-c">
-                              <div class="gjs-block gjs-one-bg gjs-four-color-h" title="Navbar" draggable="true">
-                                 <div class="gjs-block-label">
-                                    <svg class="gjs-block-svg" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                       <path class="gjs-block-svg-path" d="M22,9 C22,8.4 21.5,8 20.75,8 L3.25,8 C2.5,8 2,8.4 2,9 L2,15 C2,15.6 2.5,16 3.25,16 L20.75,16 C21.5,16 22,15.6 22,15 L22,9 Z M21,15 L3,15 L3,9 L21,9 L21,15 Z" fill-rule="nonzero"></path>
-                                       <rect class="gjs-block-svg-path" x="15" y="10" width="5" height="1"></rect>
-                                       <rect class="gjs-block-svg-path" x="15" y="13" width="5" height="1"></rect>
-                                       <rect class="gjs-block-svg-path" x="15" y="11.5" width="5" height="1"></rect>
-                                    </svg>
-                                    <div class="gjs-block-label">Navbar</div>
-                                 </div>
-                              </div>
-                              <div class="fa fa-clock-o gjs-block gjs-one-bg gjs-four-color-h" title="Countdown" draggable="true">
-                                 <div class="gjs-block-label">Countdown</div>
-                              </div>
-                           </div>
-                        </div>
-                        <div class="gjs-block-category gjs-open">
-                           <div class="gjs-title">
-                              <i class="gjs-caret-icon fa fa-caret-down"></i>
-                              Forms
-                           </div>
-                           <div class="gjs-blocks-c">
-                              <div class="gjs-block gjs-one-bg gjs-four-color-h" title="Form" draggable="true">
-                                 <div class="gjs-block-label">
-                                    <svg class="gjs-block-svg" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                       <path class="gjs-block-svg-path" d="M22,5.5 C22,5.2 21.5,5 20.75,5 L3.25,5 C2.5,5 2,5.2 2,5.5 L2,8.5 C2,8.8 2.5,9 3.25,9 L20.75,9 C21.5,9 22,8.8 22,8.5 L22,5.5 Z M21,8 L3,8 L3,6 L21,6 L21,8 Z" fill-rule="nonzero"></path>
-                                       <path class="gjs-block-svg-path" d="M22,10.5 C22,10.2 21.5,10 20.75,10 L3.25,10 C2.5,10 2,10.2 2,10.5 L2,13.5 C2,13.8 2.5,14 3.25,14 L20.75,14 C21.5,14 22,13.8 22,13.5 L22,10.5 Z M21,13 L3,13 L3,11 L21,11 L21,13 Z" fill-rule="nonzero"></path>
-                                       <rect class="gjs-block-svg-path" x="2" y="15" width="10" height="3" rx="0.5"></rect>
-                                    </svg>
-                                    <div class="gjs-block-label">Form</div>
-                                 </div>
-                              </div>
-                              <div class="gjs-block gjs-one-bg gjs-four-color-h" title="Input" draggable="true">
-                                 <div class="gjs-block-label">
-                                    <svg class="gjs-block-svg" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                       <path class="gjs-block-svg-path" d="M22,9 C22,8.4 21.5,8 20.75,8 L3.25,8 C2.5,8 2,8.4 2,9 L2,15 C2,15.6 2.5,16 3.25,16 L20.75,16 C21.5,16 22,15.6 22,15 L22,9 Z M21,15 L3,15 L3,9 L21,9 L21,15 Z"></path>
-                                       <polygon class="gjs-block-svg-path" points="4 10 5 10 5 14 4 14"></polygon>
-                                    </svg>
-                                    <div class="gjs-block-label">Input</div>
-                                 </div>
-                              </div>
-                              <div class="gjs-block gjs-one-bg gjs-four-color-h" title="Textarea" draggable="true">
-                                 <div class="gjs-block-label">
-                                    <svg class="gjs-block-svg" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                       <path class="gjs-block-svg-path" d="M22,7.5 C22,6.6 21.5,6 20.75,6 L3.25,6 C2.5,6 2,6.6 2,7.5 L2,16.5 C2,17.4 2.5,18 3.25,18 L20.75,18 C21.5,18 22,17.4 22,16.5 L22,7.5 Z M21,17 L3,17 L3,7 L21,7 L21,17 Z"></path>
-                                       <polygon class="gjs-block-svg-path" points="4 8 5 8 5 12 4 12"></polygon>
-                                       <polygon class="gjs-block-svg-path" points="19 7 20 7 20 17 19 17"></polygon>
-                                       <polygon class="gjs-block-svg-path" points="20 8 21 8 21 9 20 9"></polygon>
-                                       <polygon class="gjs-block-svg-path" points="20 15 21 15 21 16 20 16"></polygon>
-                                    </svg>
-                                    <div class="gjs-block-label">Textarea</div>
-                                 </div>
-                              </div>
-                              <div class="gjs-block gjs-one-bg gjs-four-color-h" title="Select" draggable="true">
-                                 <div class="gjs-block-label">
-                                    <svg class="gjs-block-svg" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                       <path class="gjs-block-svg-path" d="M22,9 C22,8.4 21.5,8 20.75,8 L3.25,8 C2.5,8 2,8.4 2,9 L2,15 C2,15.6 2.5,16 3.25,16 L20.75,16 C21.5,16 22,15.6 22,15 L22,9 Z M21,15 L3,15 L3,9 L21,9 L21,15 Z" fill-rule="nonzero"></path>
-                                       <polygon class="gjs-block-svg-path" transform="translate(18.500000, 12.000000) scale(1, -1) translate(-18.500000, -12.000000) " points="18.5 11 20 13 17 13"></polygon>
-                                       <rect class="gjs-block-svg-path" x="4" y="11.5" width="11" height="1"></rect>
-                                    </svg>
-                                    <div class="gjs-block-label">Select</div>
-                                 </div>
-                              </div>
-                              <div class="gjs-block gjs-one-bg gjs-four-color-h" title="Button" draggable="true">
-                                 <div class="gjs-block-label">
-                                    <svg class="gjs-block-svg" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                       <path class="gjs-block-svg-path" d="M22,9 C22,8.4 21.5,8 20.75,8 L3.25,8 C2.5,8 2,8.4 2,9 L2,15 C2,15.6 2.5,16 3.25,16 L20.75,16 C21.5,16 22,15.6 22,15 L22,9 Z M21,15 L3,15 L3,9 L21,9 L21,15 Z" fill-rule="nonzero"></path>
-                                       <rect class="gjs-block-svg-path" x="4" y="11.5" width="16" height="1"></rect>
-                                    </svg>
-                                    <div class="gjs-block-label">Button</div>
-                                 </div>
-                              </div>
-                              <div class="gjs-block gjs-one-bg gjs-four-color-h" title="Label" draggable="true">
-                                 <div class="gjs-block-label">
-                                    <svg class="gjs-block-svg" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                       <path class="gjs-block-svg-path" d="M22,11.875 C22,11.35 21.5,11 20.75,11 L3.25,11 C2.5,11 2,11.35 2,11.875 L2,17.125 C2,17.65 2.5,18 3.25,18 L20.75,18 C21.5,18 22,17.65 22,17.125 L22,11.875 Z M21,17 L3,17 L3,12 L21,12 L21,17 Z" fill-rule="nonzero"></path>
-                                       <rect class="gjs-block-svg-path" x="2" y="5" width="14" height="5" rx="0.5"></rect>
-                                       <polygon class="gjs-block-svg-path" fill-rule="nonzero" points="4 13 5 13 5 16 4 16"></polygon>
-                                    </svg>
-                                    <div class="gjs-block-label">Label</div>
-                                 </div>
-                              </div>
-                              <div class="fa fa-check-square gjs-block gjs-one-bg gjs-four-color-h" title="Checkbox" draggable="true">
-                                 <div class="gjs-block-label">Checkbox</div>
-                              </div>
-                              <div class="fa fa-dot-circle-o gjs-block gjs-one-bg gjs-four-color-h" title="Radio" draggable="true">
-                                 <div class="gjs-block-label">Radio</div>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
+                     </div>   
                      <div class="gjs-blocks-no-cat">
                         <div class="gjs-blocks-c">
                            <div class="gjs-block gjs-one-bg gjs-four-color-h" title="Auto Complete" draggable="true">
@@ -352,7 +224,7 @@ export default {
    data() {
       return {
          selectedPanel: "Components",
-         componentsGroups: this.cGroups,
+         componentsGroups: this.componentsPaletteGroups,
       };
    },
    methods: {
@@ -371,10 +243,35 @@ export default {
       // load the page layout
       let layout = layoutLoader.loadLayout()
 
-      let cGroups = new Array()
+      let componentsPaletteGroups = new Array()
 
       // Available Components
-      cGroups.push({ title: 'Basic', isOpen: true })
+      componentsPaletteGroups.push({ 
+         title: 'Basic', 
+         isOpen: true,
+         components: [
+            { id:'ContainerOneColumn', title: '1 Column ', classes: 'gjs-fonts gjs-f-b1' },
+            { id:'ContainerTwoColumns', title: '2 Columns', classes: 'gjs-fonts gjs-f-b2' },
+            { id:'ContainerThreeColumns', title: '3 Columns', classes: 'gjs-fonts gjs-f-b3' },
+            { id:'ContainerTwoColumn3-7', title: '2 Columns 3/7', classes: 'gjs-fonts gjs-f-b37' },
+            { id:'Text', title: 'Text', classes: 'gjs-fonts gjs-f-text' },
+            { id:'Link', title: 'Link', classes: 'fa fa-link' },
+            { id:'Image', title: 'Image', classes: 'gjs-fonts gjs-f-image' },
+            { id:'Video', title: 'Video', classes: 'fa fa-youtube-play' },
+            { id:'Map', title: 'Map', classes: 'fa fa-map-o' },
+            { id:'LinkBlock', title: 'Link Block', classes: 'fa fa-link' },
+            { id:'Quote', title: 'Quote', classes: 'fa fa-quote-right' },
+            { id:'TextSection', title: 'Text section', classes: 'gjs-fonts gjs-f-h1p' },
+         ]
+       })
+      componentsPaletteGroups.push({ 
+         title: 'Extra', 
+         isOpen: true,
+         components: [
+            { id:'Navbar', title: 'Navbar', innerHtml: '<svg class="gjs-block-svg" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" data-v-46facb22=""><path class="gjs-block-svg-path" d="M22,9 C22,8.4 21.5,8 20.75,8 L3.25,8 C2.5,8 2,8.4 2,9 L2,15 C2,15.6 2.5,16 3.25,16 L20.75,16 C21.5,16 22,15.6 22,15 L22,9 Z M21,15 L3,15 L3,9 L21,9 L21,15 Z" fill-rule="nonzero" data-v-46facb22=""></path><rect class="gjs-block-svg-path" x="15" y="10" width="5" height="1" data-v-46facb22=""></rect><rect class="gjs-block-svg-path" x="15" y="13" width="5" height="1" data-v-46facb22=""></rect><rect class="gjs-block-svg-path" x="15" y="11.5" width="5" height="1" data-v-46facb22=""></rect></svg>' },
+            { id:'Countdown', title: 'Countdown', classes: 'fa fa-clock-o' },
+         ]
+       })       
 
       const { appContext } = getCurrentInstance();
       const css = ref(
@@ -471,7 +368,7 @@ export default {
          layout,
          css,
          appContext,
-         cGroups
+         componentsPaletteGroups
       };
    },
 };
